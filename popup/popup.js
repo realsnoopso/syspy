55864212df2331de9e3739184538f1ed21e41fb3
realsnoopso <real@snoop.so>
2023-05-14 01:05:54 +0900

    Load all typography

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,15 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'FONTS_AND_STYLES') {
+    const { colors, typography, fontFaceRules } = message.data;
+
+    // (생략된 코드: colors, typography를 사용하여 popup 창의 내용을 업데이트)
+
+    const styleTag = document.createElement('style');
+    styleTag.textContent = fontFaceRules.join('\n');
+    document.head.appendChild(styleTag);
+  }
+});
+
 const getColorsFromStorage = async () => {
   return new Promise((resolve) => {
     chrome.storage.local.get('colors', (result) => {
@@ -6,36 +18,112 @@ const getColorsFromStorage = async () => {
   });
 };
 
+const getTypographyFromStorage = async () => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get('typography', (result) => {
+      resolve(result.typography || []);
+    });
+  });
+};
+
+function rgbaStringToHex(rgbaString) {
+  if (typeof rgbaString !== 'string') {
+    throw new Error('Input must be a string');
+  }
+
+  const rgbArray = rgbaString
+    .substring(rgbaString.indexOf('(') + 1, rgbaString.lastIndexOf(')'))
+    .split(',')
+    .map((value) => value.trim());
+
+  const [r, g, b] = rgbArray;
+
+  const red = parseInt(r, 10).toString(16).padStart(2, '0');
+  const green = parseInt(g, 10).toString(16).padStart(2, '0');
+  const blue = parseInt(b, 10).toString(16).padStart(2, '0');
+
+  return `#${red}${green}${blue}`;
+}
+
 const displayColors = (color) => {
   const colorsContainer = document.querySelector('#colorsContainer');
   const element = document.createElement('div');
   element.classList.add('color-block');
   element.style.backgroundColor = color;
+  const hex = rgbaStringToHex(color);
 
-  console.log(element);
-  console.log(colorsContainer);
+  element.innerText = hex;
+
   colorsContainer.append(element);
 };
 
-document
-  .getElementById('fetchColorsBtn')
-  .addEventListener('click', async () => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
+const displayTypography = (typography) => {
+  const {
+    fontFamily,
+    fontSize,
+    fontWeight,
+    lineHeight,
+    letterSpacing,
+    typoType,
+  } = typography;
+
+  const colorsContainer = document.querySelector('#typosContainer');
+  const element = document.createElement('div');
+  const title = document.createElement('h3');
+
+  title.innerText =
+    typoType === 'title'
+      ? '타이틀입니다'
+      : typoType === 'button'
+      ? '버튼'
+      : lineHeight === '0px'
+      ? '짧은 텍스트'
+      : '긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다. 긴 텍스트입니다.';
+
+  title.style.fontFamily = fontFamily;
+  title.style.fontSize = fontSize;
+  title.style.fontWeight = fontWeight;
+  title.style.lineHeight = lineHeight;
+  title.style.letterSpacing = letterSpacing;
+  const contents = document.createElement('p');
+  element.classList.add('typo-block');
+  contents.innerText = `fontFamily: ${fontFamily.split(',')[0].trim()},
+  fontSize: ${fontSize},
+  fontWeight: ${fontWeight},
+  lineHeight: ${lineHeight},
+  letterSpacing: ${letterSpacing},
+  `;
+
+  element.append(title, contents);
+  colorsContainer.append(element);
+};
+
+const renderSystem = async () => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['scripts/contentScript.js'],
+    });
+    const colors = await getColorsFromStorage();
+    const typography = await getTypographyFromStorage();
+
+    colors.forEach((color) => {
+      displayColors(color);
+    });
+    typography.forEach((typography) => {
+      displayTypography(typography);
     });
 
-    try {
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['scripts/contentScript.js'],
-      });
-      const colors = await getColorsFromStorage();
+    const contents = document.querySelector('#contents');
+    contents.classList.add('show');
+  } catch (error) {
+    console.error('Error executing script:', error);
+  }
+};
 
-      colors.forEach((color) => {
-        displayColors(color);
-      });
-    } catch (error) {
-      console.error('Error executing script:', error);
-    }
-  });
+renderSystem();
--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,11 +1,144 @@
-async function fetchColors() {
+function rgbToHsl(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0;
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, l];
+}
+
+async function fetchFontsData() {
+  const fontFaceRules = [];
+  const styleSheets = document.styleSheets;
+
+  async function fetchStylesheet(url) {
+    try {
+      const response = await fetch(url);
+      const text = await response.text();
+      return text;
+    } catch (error) {
+      console.error('Error while fetching stylesheet:', error);
+      return null;
+    }
+  }
+
+  async function findFontFaceRulesInText(text) {
+    const regex = /@font-face\s*{[^}]*}/g;
+    const foundRules = text.match(regex);
+    if (foundRules) {
+      fontFaceRules.push(...foundRules);
+    }
+  }
+
+  for (const styleSheet of styleSheets) {
+    try {
+      if (styleSheet.href) {
+        const stylesheetText = await fetchStylesheet(styleSheet.href);
+        if (stylesheetText) {
+          await findFontFaceRulesInText(stylesheetText);
+        }
+      } else {
+        const cssRules = styleSheet.cssRules;
+        for (const rule of cssRules) {
+          if (rule.type === CSSRule.FONT_FACE_RULE) {
+            fontFaceRules.push(rule.cssText);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error while accessing styleSheet:', error);
+    }
+  }
+
+  return fontFaceRules;
+}
+
+(async () => {
+  const { uniqueColors, uniqueTypography } = await fetchElementsData();
+  const fontFaceRules = await fetchFontsData();
+
+  chrome.runtime.sendMessage({
+    type: 'FONTS_AND_STYLES',
+    data: {
+      colors: uniqueColors,
+      typography: uniqueTypography,
+      fontFaceRules: fontFaceRules,
+    },
+  });
+})();
+
+function sortByLightness(a, b) {
+  const aRgb = a.match(/\d+/g).map(Number);
+  const bRgb = b.match(/\d+/g).map(Number);
+  const [, , aLightness] = rgbToHsl(...aRgb);
+  const [, , bLightness] = rgbToHsl(...bRgb);
+
+  return bLightness - aLightness;
+}
+
+function sortByColor(a, b) {
+  return sortByLightness(a, b);
+}
+
+function sortByFontSize(a, b) {
+  const aSize = parseFloat(a.fontSize);
+  const bSize = parseFloat(b.fontSize);
+
+  return bSize - aSize;
+}
+
+function sortByTypoType(a, b) {
+  const order = { title: 1, body: 2, button: 3 };
+  return order[a.typoType] - order[b.typoType];
+}
+
+function removeDuplicateObjects(arr) {
+  const seen = new Set();
+  return arr.filter((item) => {
+    const k = JSON.stringify(item);
+    return seen.has(k) ? false : seen.add(k);
+  });
+}
+
+async function fetchElementsData() {
   const elements = document.querySelectorAll('*');
   const colors = [];
+  const typography = [];
 
   for (let i = 0; i < elements.length; i++) {
+    const tagName = elements[i].tagName.toLowerCase();
     const style = window.getComputedStyle(elements[i]);
     const color = style.getPropertyValue('color');
     const bgColor = style.getPropertyValue('background-color');
+    const fontFamily = style.getPropertyValue('font-family');
+
+    const fontSize = style.getPropertyValue('font-size');
+    const fontWeight = style.getPropertyValue('font-weight');
+    const lineHeight = style.getPropertyValue('line-height');
+    const letterSpacing = style.getPropertyValue('letter-spacing');
 
     if (color && color !== 'transparent' && color !== 'rgba(0, 0, 0, 0)') {
       colors.push(color);
@@ -17,15 +150,39 @@ async function fetchColors() {
     ) {
       colors.push(bgColor);
     }
+
+    const titleRegex = /^h[1-6]$/i; // 대소문자 구분 없이 변경합니다.
+    const typoType = titleRegex.test(tagName)
+      ? 'title'
+      : tagName === 'button'
+      ? 'button'
+      : 'body';
+
+    const currentTypography = {
+      typoType: typoType,
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      lineHeight: lineHeight,
+      letterSpacing: letterSpacing,
+    };
+
+    typography.push(currentTypography);
   }
 
-  const uniqueColors = Array.from(new Set(colors));
-  return uniqueColors;
+  const uniqueColors = Array.from(new Set(colors)).sort(sortByColor);
+  const uniqueTypography = removeDuplicateObjects(typography)
+    .sort(sortByFontSize)
+    .sort(sortByTypoType); // typoType 기준으로 정렬 추가
+  return { uniqueColors, uniqueTypography };
 }
 
 const saveToChromeStorage = async () => {
-  const colors = await fetchColors();
-  chrome.storage.local.set({ colors: colors });
+  const { uniqueColors, uniqueTypography } = await fetchElementsData();
+  chrome.storage.local.set({
+    colors: uniqueColors,
+    typography: uniqueTypography,
+  });
 };
 
 saveToChromeStorage();
